69f945495a80f2caa7f50a85aad0526ab150c24e
Zach Roehl <zroehl19@gmail.com>
2017-09-25 14:12:24 -0400

    Before

---
+++ Lab 4.5.1/main.cpp
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: zroehl
+ *
+ * Created on September 25, 2017, 1:31 PM
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main() {
+    string sentence;
+    getline(cin, sentence);
+    
+    
+    
+    cout << sentence << "\n";
+}
+
